2485f96aca4744610a31d0b14e5315062900f759
MarcoDanielAcciarri <75322669+MarcoDanielAcciarri@users.noreply.github.com>
2020-12-05 03:46:21 -0300

    agrego version movil

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ $(document).ready(function(){
     // Efecto Rebote
     $("nav a").on('click', function(){
         var target = $(this).attr('href');
-        $(target).animate({paddingTop: '4rem'},1000);
+        $(target).animate({paddingTop: '8rem'},1000);
     });
     // Fin Efecto Rebote
+
+    $('.toggle-menu').on('click', function(){
+        //$('#small-menu').toggleClass('mostrar');
+        myFunction();
+    })
+
+    function myFunction() {
+        var x = document.getElementById("small-menu");
+        if (x.style.display == "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
    
 })
 
